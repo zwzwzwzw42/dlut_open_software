225f3cc4426b7ee34d8f7dc76d35fc8371236f33
xuhao_liyoh <2841819826@qq.com>
2025-01-24 13:50:58 +0800

    徐昊平时作业

--- 徐昊_平时作业/test1.py
+++ 徐昊_平时作业/test1.py
@@ -1,41 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-def foo(bar, buz):
-    if bar > buz:
-        bar += buz
-    else:
-        buz *= bar
-
-    return bar + buz
-
-foo(3, 4)
-foo(5, 4)
-
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
-from flask import Flask
-
-app = Flask(__name__) # 'hello'
-
-@app.route("/<name>")
-def hello(name):
-    return f"""<!DOCTYPE html>
-<html>
-    <body>
-        <p>hello world, <b>{name}</b></p>
-    </body>
-</html>"""
-
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
 from flask import Flask, render_template
 
 app = Flask(__name__) # 'hello'
@@ -43,4 +8,15 @@ app = Flask(__name__) # 'hello'
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello(name=None):
+    return render_template("index.html", name=name)
+#!/usr/bin/env python
+# coding: utf-8
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__) # 'hello'
+
+@app.route("/hello") # localhost:5000/hello?name=zhangsan
+def hello():
+    name = request.args.get("name", "anonymous")
     return render_template("index.html", name=name)
